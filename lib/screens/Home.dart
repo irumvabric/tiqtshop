7bdb7cd1c9ae36830202183f27666d3e2a3405cb
irumvabric <irumvabric#@gmail.com>
2024-09-19 20:29:00 +0200

    Bottom nav implemented

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -4,28 +4,59 @@ import 'package:tiqtshop/widgets/bottom_navigation.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:tiqtshop/screens/Verified.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  // List of pages for each BottomNavigationBar item
+  final List<Widget> _pages = [
+    Center(child: Text("Home Page")),
+    Barcode_scanner(),
+    VerificationPage(),
+    VerificationPage(),
+  ];
+
+  // Function to handle BottomNavigationBar item taps
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Remove the Scaffold from here
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const VerificationPage(),
-                ),
-              );
-            },
-            child: const Text("Mobile Scanner Simple"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _pages[_selectedIndex], // Display the current selected page
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped, 
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.airplane_ticket_rounded),
+            label: 'Ticket',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.airplane_ticket_rounded),
+            label: 'Profile',
           ),
         ],
       ),
